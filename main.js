de82e886fb3ca4fa8de8548b6fdc17dda441f5c7
Zii09 <zaidanikhlasul.99@gmail.com>
2020-09-30 20:58:40 +0700

    Domevent

---
+++ main.js
@@ -0,0 +1,23 @@
+//kita buat sebuah fungsi untuk
+//memunculkan MODAL
+
+const showModal = () => {
+    //kita dapatkan element MODAL
+    //yang mau di munculkan
+    let modal = document.getElementById('modalMenu')
+
+    //kita atur styling element modalnya
+    modal.style.display = 'flex'
+
+}
+`1  q`
+const closeModal = () => {
+    //kita dapatkan element MODAL
+    //yang mau di munculkan
+    let modal = document.getElementById('modalMenu')
+
+    //kita atur styling element modalnya
+    modal.style.display = 'none'
+
+}
+
