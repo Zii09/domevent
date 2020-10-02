6386f746e507609d0615d4f2251e60d9f5896686
Zii09 <zaidanikhlasul.99@gmail.com>
2020-10-02 23:10:23 +0700

    Zaidan

--- main.js
+++ main.js
@@ -21,3 +21,16 @@ const closeModal = () => {
 
 }
 
+//kita ambil alamat by id
+let showcase = document.getElementById("wrapper_showcase")
+
+// kita inject elemntnya dengan sytax HTML
+//showcase.innerHTML += `
+//<img src="https://picsum.photos/seed/9/300" alt="ini Gambar" />
+//<h1 style='color : white'>Selamat Datang ${nama}</h1>`
+
+for (var i = 0; i < 9; i++) {
+    showcase.innerHTML += `
+    <img src="https://picsum.photos/seed/${1 + i}/235" alt='ini Gambar ke 1' />
+    `
+}
